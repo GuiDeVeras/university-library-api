68a3d4bbab5c9fe849b69887053d9546df3a1c97
GuilhermeDeVeras <guilhermepinheirodeveras@gmail.com>
2026-07-13 09:48:46 -0300

    chore: initialize Maven project

---
+++ src/main/java/api/Server.java
@@ -0,0 +1,20 @@
+package api;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.start();
+
+        System.out.println("Server started at http://localhost:8080");
+
+    }
+
+}
