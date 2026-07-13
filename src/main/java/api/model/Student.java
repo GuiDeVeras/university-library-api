462e7ef359dfb87a2f20ff1959a63323b5562c59
GuilhermeDeVeras <guilhermepinheirodeveras@gmail.com>
2026-07-13 10:15:33 -0300

    feat: add student model

---
+++ src/main/java/api/model/Student.java
@@ -0,0 +1,62 @@
+package api.model;
+
+public class Student {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String registration;
+    private String course;
+
+    public Student() {
+    }
+
+    public Student(Integer id, String name, String email, String registration, String course) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.registration = registration;
+        this.course = course;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+}
